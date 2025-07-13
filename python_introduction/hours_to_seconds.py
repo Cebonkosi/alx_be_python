d73c62282f761deef6a0a006e66961a4a524cd4b
Cebolenkosi Gumpo <nkosig467@gmail.com>
2025-07-13 19:43:58 +0200

    Move Python scripts into python_introduction directory

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,8 @@
+# Define the number of hours
+hours = 2
+
+# Calculate the number of seconds
+seconds = hours * 60 * 60
+
+# Output the result
+print(hours, "hour(s) is", seconds, "seconds.")
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Define the variables
+principal = 1000      # $1000 principal
+rate = 0.05           # 5% interest rate
+time = 3              # 3 years
+
+# Calculate simple interest
+interest = principal * rate * time
+
+# Output the result
+print("The simple interest is:", interest)
