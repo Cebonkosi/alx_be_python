ed74d53d1a99f5acf4445249c4d872dc8ea9842c
Cebolenkosi Gumpo <nkosig467@gmail.com>
2025-08-03 17:12:38 +0200

    test_simple_calculator

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,39 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Create a SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, -5), -6)
+        self.assertEqual(self.calc.add(-2, 2), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(10, 4), 6)
+        self.assertEqual(self.calc.subtract(-3, -6), 3)
+        self.assertEqual(self.calc.subtract(5, 10), -5)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(3, 5), 15)
+        self.assertEqual(self.calc.multiply(-2, 4), -8)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+        self.assertEqual(self.calc.multiply(-3, -3), 9)
+
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-9, 3), -3)
+        self.assertEqual(self.calc.divide(5, -1), -5)
+        self.assertIsNone(self.calc.divide(8, 0))  # Division by zero should return None
+
+    def test_division_floats(self):
+        self.assertAlmostEqual(self.calc.divide(7, 3), 2.3333333, places=6)
+        self.assertAlmostEqual(self.calc.divide(1, 4), 0.25)
+
+if __name__ == "__main__":
+    unittest.main()
